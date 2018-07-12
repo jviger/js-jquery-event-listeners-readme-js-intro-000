ccab335b03cd7344b17796559057378dfc78ba3a
Jason Viger <jason.viger@gmail.com>
2018-07-12 12:04:23 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,5 +1,12 @@
 //define functions here
-
+function getIt(){
+  $('p').alert('Hey!');
+}
+function frameIt(){
+  $('img').on('load',function(){
+    
+  });
+}
 $(document).ready(function(){
 
 // call functions here
